cd7223e87492d1d973f94bcea5e8cb08f65db81a
rohittuk07 <rohitchauhan93899@gmail.com>
2024-09-10 11:46:42 +0530

    Routing Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
-import './App.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { About, Cart, Checkout, Error, HomeLayout, Landing, Login, Orders, Products, Register, SingleProduct } from './Pages'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<HomeLayout/>,
+      errorElement:<Error/>,
+      children: [
+        {
+          index:true,
+          element:<Landing/>
+        },
+        {
+          path:'about',
+          element:<About/>
+        },
+        {
+          path:'products',
+          element:<Products/>
+        },
+        {
+          path:'orders',
+          element:<Orders/>
+        },
+        {
+          path:'cart',
+          element:<Cart/>
+        },
+        {
+         path:'checkout',
+         element:<Checkout/>
+        },
+        {
+          path:'singleproduct',
+          element:<SingleProduct/>
+        }
+      ]
+    },
+    {
+      path:'/login',
+      element:<Login/>
+    },
+    {
+      path:'/register',
+      element:<Register/>
+    }
+  ])
 
   return (
-    <>
-    <h2 className='text-red-400 text-3xl font-extrabold text-center'>Hello</h2>
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/Pages/Error.jsx
@@ -0,0 +1,4 @@
+const Errors = () => {
+    return <h1 className='text-4xl'>Errors</h1>;
+  };
+  export default Errors;
